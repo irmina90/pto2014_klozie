6a7b4d221ccb46fbc46d010cc9c537c34b2b5ced
Marta Kłobukowska <mklobukowska@gmail.com>
2014-10-29 23:31:01 +0100

    Zad.4 - cz.3 Gaussowskie rozmywanie obrazu

--- src/core/transformations/blur_gaussian.cpp
+++ src/core/transformations/blur_gaussian.cpp
@@ -25,15 +25,19 @@ math::matrix<float> BlurGaussian::getMask(int size, Mode)
 {
     math::matrix<float> mask(size, size);
 
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
+    for (int i=0; i<size; i++)
+         for (int j=0; j<size; j++)
+         {
+            mask(i, j) = getGauss(i, j, sigma);
+         }
 
     return mask;
 }
 
 float BlurGaussian::getGauss(int x, int y, float sigma)
 {    
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
-
-    return 0;
+    float fraction = (1 / (2 * M_PI * pow(sigma, 2)));
+    float powerE = pow(M_E, (-1) * (pow(x, 2) + pow(y, 2)) / (2 * pow(sigma, 2)));
+    return fraction * powerE;
 }
 
