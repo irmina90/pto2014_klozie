8635ad27f089345e4e9b65e40177168517df388c
irmina90 <s362704@TERM2014Z.labs.wmi.amu.edu.pl>
2014-11-26 18:26:21 +0100

    zad.8 Wykrywanie krawedzi - cz.1

--- src/core/transformations/edge_gradient.cpp
+++ src/core/transformations/edge_gradient.cpp
@@ -22,9 +22,53 @@ PNM* EdgeGradient::horizontalDetection()
 
 PNM* EdgeGradient::transform()
 {
+
     PNM* newImage = new PNM(image->width(), image->height(), image->format());
 
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
+    int width = image->width();
+    int height = image->height();
+
+    PNM* xImage = horizontalDetection();
+    PNM* yImage = verticalDetection();
+
+    if (image->format() == QImage::Format_Indexed8)
+    {
+        for (int i=0; i<width; i++)
+        {
+            for (int j=0; j<height; j++)
+            {
+                int xPix = qGray(xImage->pixel(i,j));
+                int yPix = qGray(yImage->pixel(i,j));
+                int newPix = sqrt(xPix*xPix + yPix*yPix);
+                newImage->setPixel(i,j,newPix);
+            }
+        }
+    }
+    else
+    {
+        for (int i=0; i<width; i++)
+        {
+            for (int j=0; j<height; j++)
+            {
+                int rX = qRed(xImage->pixel(i,j));
+                int rY = qRed(yImage->pixel(i,j));
+
+                int gX = qGreen(xImage->pixel(i,j));
+                int gY = qGreen(yImage->pixel(i,j));
+
+                int bX = qBlue(xImage->pixel(i,j));
+                int bY = qBlue(yImage->pixel(i,j));
+
+                int r = sqrt(rX*rX + rY*rY);
+                int g = sqrt(gX*gX + gY*gY);
+                int b = sqrt(bX*bX + bY*bY);
+
+                newImage->setPixel(i,j,qRgb(r,g,b));
+            }
+        }
+
+    }
+
 
     return newImage;
 }
