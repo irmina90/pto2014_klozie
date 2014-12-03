e2ec54d2262a760bfdae79b270a60b5f0f27b526
Marta Kłobukowska <mklobukowska@gmail.com>
2014-12-03 19:41:16 +0100

    Zad.9 - generowanie mapy horyzontu

--- src/core/transformations/map_horizon.cpp
+++ src/core/transformations/map_horizon.cpp
@@ -35,7 +35,41 @@ PNM* MapHorizon::transform()
 
     PNM* newImage = new PNM(width, height, QImage::Format_Indexed8);
 
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
+    PNM* mapHeight = MapHeight(image).transform();
+
+    for(int i=0; i<width; i++)
+        for(int j=0; j<height; j++)
+        {
+           double alpha = 0;
+           int current_h = qRed(mapHeight->pixel(i, j));
+
+           for(int k = i+dx, l = j+dy; k < width && l < height && k >= 0 && l >= 0; k = k+dx, l = l+dy)
+           {
+             int ray_h = qRed(mapHeight->pixel(k,l));
+
+             if(current_h < ray_h)
+             {
+               double dist = sqrt(pow(k - i, 2) + pow(l - j, 2)) * scale;
+               double ray_alpha = atan((ray_h - current_h) / dist);
+
+               if(ray_alpha > alpha) alpha = ray_alpha;
+
+             }
+
+            }
+
+            double delta = alpha - sun_alpha * M_PI/180;
+
+            if(delta > 0)
+            {
+              newImage->setPixel(i, j, cos(delta) * 255);
+            }
+            else
+            {
+              newImage->setPixel(i, j, 255);
+            }
+
+         }
 
     return newImage;
 }
